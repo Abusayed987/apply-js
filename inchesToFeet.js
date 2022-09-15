3772ca7698efbc353523e8907107f09156927c6f
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-15 11:48:05 +0600

    first commit

---
+++ inchesToFeet.js
@@ -0,0 +1,74 @@
+// p:01 inches to feet
+
+function inchesToFeet(inches) {
+   const feet = inches / 12;
+   return feet;
+}
+const YourInches = 144;
+const YourFeet = inchesToFeet(YourInches);
+console.log('your Feet:', YourFeet);
+
+
+// p:02 inches to feet
+function inchesToFeet(inches){
+   const feet = inches/12;
+   return feet;
+}
+const dadarInches = 156;
+const dadarFeet = inchesToFeet(dadarInches)
+console.log('Dadar Feet:', dadarFeet);
+
+// p:03 inches to feet
+function inchesToFeet(inches){
+   const feet = inches/12;
+   return feet;
+}
+const dadirInches = 48;
+const dadirfeet = inchesToFeet(dadirInches)
+console.log('Dadir Inches:',dadirfeet);
+
+// p:04 inches to feet
+function  inchesToFeet (inches){
+   const feet = inches/12;
+   return feet;
+}
+const nanarInches = 80;
+const nanarfeet = inchesToFeet(nanarInches)
+console.log('Nanar Feet: ', nanarfeet);
+
+
+// p:05 inches to feet
+function inchesToFeet(inches){
+   const feet = inches/12;
+   return feet;
+}
+const nanirInches = 70;
+const nanirFeeet = inchesToFeet(nanirInches);
+console.log('Nanir Feet: ',nanirFeeet);
+
+// practice problem 01: mile to kilomiter
+function mileToKilo(mile) {
+   const kilo = mile *  1.60934;
+   return kilo
+}
+const homeToBazarMile = 1;
+const YourResult = mileToKilo(homeToBazarMile);
+console.log('Home to Bazar: ', YourResult, 'kilo');
+
+// practice problem 02: mile to kilomiter
+function homeToNanaBari(mile) {
+    const kilo = mile * 1.60934;
+    return kilo
+}
+const homeToNanaBariMile = 1.5;
+const homeToNanaBariKilo = homeToNanaBari(homeToNanaBariMile);
+console.log('Home to Nana Bari Kilo:', homeToNanaBariKilo, 'kilo');
+
+// practice problem 03: mile to kilomiter
+function homeToMuladi(mile) {
+    const kilo = mile* 1.60934;
+    return kilo
+}
+const homeToMuladiMile = 12;
+const homeToMuladikilo = homeToMuladi(homeToMuladiMile);
+console.log('home To Muladi kilo:', homeToMuladikilo, 'kilo')
