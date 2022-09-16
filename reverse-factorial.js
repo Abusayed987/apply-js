645fb0934ffb5db846b9d7022b413013f19b09cd
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-16 15:02:05 +0600

    add folder

---
+++ reverse-factorial.js
@@ -0,0 +1,21 @@
+// practice: 01 Use for loop
+function reverseFactorial(num) {
+    let result = 1;
+    for (let i = num; i >= i; i++) {
+        result = result * i;
+    }
+    return result
+}
+const result = reverseFactorial(7);
+
+
+// practice: 02 Use for loop
+// practice: 03 Use for loop
+// practice: 04 Use for loop
+// practice: 05 Use for loop
+
+// practice :01 use while loop
+// practice :02 use while loop
+// practice :03 use while loop
+// practice :04 use while loop
+// practice :05 use while loop
